8dc848457777cfef12d7d98bc67b460f049a20f3
Viet <viet.dang@dirox.net>
2022-11-01 22:56:30 +0700

    Create the basic CoachesList

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachDetail from './pages/coaches/CoachesDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import CoachRegistration from './pages/coaches/CoachesRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
