cdb11f9367867a946b241218064e08504758a893
Viet <viet.dang@dirox.net>
2022-11-05 12:59:25 +0700

    Added styling for the CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,3 +6,19 @@
         LIST OF COACHES
     </section>
 </template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
