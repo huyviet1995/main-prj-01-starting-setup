50276d5a012d01e0c8f388e288499419ae91e416
Viet <viet.dang@dirox.net>
2022-11-05 13:21:00 +0700

    Adding CoachesList, CoachesRegistration, ContactCoaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,7 +3,13 @@
         FILTER
     </section>
     <section>
-        LIST OF COACHES
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as coach</router-link>
+        </div>
+        <ul>
+            LIST OF COACHES
+        </ul>
     </section>
 </template>
 
