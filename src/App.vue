b86961ebbf8ccff33f84ae6fc669a2ac7dfd09ff
Viet <viet.dang@dirox.net>
2022-11-05 13:08:31 +0700

    Added Header to the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
     <router-view></router-view>
+    <the-header></the-header>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  }
+}
+</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
