3c09f9e98e371ced82c01979628b492bd9af4652
Viet <viet.dang@dirox.net>
2022-11-06 15:44:09 +0700

    Create and register the base card component and use it to beautify the style of the component

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
 import store from './store/index';
+import BaseCard from './components/ui/BaseCard.vue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
 
+app.component('base-card', BaseCard);
 app.mount('#app')
