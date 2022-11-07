2314fd4d609cc3eda9ccf4e61cd1dbd9e9a7dc65
Viet <viet.dang@dirox.net>
2022-11-07 21:53:32 +0700

    Added the request received and request item component and its corresponding logic

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,18 +1,46 @@
 <template>
-    <form action="">
+    <form @submit.prevent="submitForm" action="">
         <div class="form-control">
             <label for="email">You E-Mail</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" v-model.trim="email" />
         </div>
         <div clas="form-control">
             <label for="message">Message</label>
-            <textarea rows="5" id="message"></textarea>
+            <textarea rows="5" id="message" v-model.trim="message"></textarea>
         </div>
+        <p class="errors" v-if="!formIsValid">Please enter a valid email and non-empty message</p>
         <div class="actions">
             <base-button>Send message</base-button>
         </div>
     </form>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            message: '',
+            formIsValid: true,
+        }
+    },
+    methods: {
+        submitForm() {
+            this.formIsValid = true;
+            if (this.email === '' || !this.email.includes('@') || this.message === '') {
+                this.formIsValid = false;
+                return;
+            }
+            this.$store.dispatch('requests/contactCoach', {
+                email: this.email,
+                message: this.message,
+                coachId: this.$route.params.id,
+            });
+            this.$router.replace('/coaches');
+        }
+    }
+}
+</script>
 
 <style scoped>
 form {
