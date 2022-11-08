313a808ead52daa98f164950cfb308c4f9d4398f
Viet <viet.dang@dirox.net>
2022-11-08 22:08:02 +0700

    Loaded coaches from the server

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -20,12 +20,34 @@ export default {
             // error
         ]
 
-
-
         context.commit('registerCoach', { 
             ...coachData,
             id: userId
         });
-    }
+    },
+    // Loading the coaches.
+    async loadCoaches(context)  {
+        const response = await fetch(`https://vue-project-a031a-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`);
+        const responseData = await response.json();
 
-}
+        if (!response.ok) {
+            // ...
+        }
+
+        const coaches = context.getters.coaches;
+
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas,
+            }
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches);
+    }
+}           
