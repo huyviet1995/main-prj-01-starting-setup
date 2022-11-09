03967dbd4704c3989f1ff15e2ef40d90f248596c
Viet <viet.dang@dirox.net>
2022-11-09 22:41:31 +0700

    Added transition to all the pages in vue

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,34 +1,29 @@
 <template>
-  <base-dialog :show="!!error" @close="handleError">
-    <p>{{ error }}</p>
-  </base-dialog>
-  <section>
-    <coach-filter @change-filter="setFilters"></coach-filter>
-  </section>
-  <section>
-    <base-card>
-      <div class="controls">
-        <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register">Register as coach</base-button>
-      </div>
-      <div v-if="isLoading">
-        <base-spinner></base-spinner>
-      </div>
-      <ul v-else-if="hasCoaches">
-        <coach-item
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :first-name="coach.firstName"
-          :last-name="coach.lastName"
-          :rate="coach.hourlyRate"
-          :areas="coach.areas"
-        >
-        </coach-item>
-      </ul>
-      <h3 v-else>No coaches found</h3>
-    </base-card>
-  </section>
+  <div>
+    <base-dialog :show="!!error" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section>
+      <coach-filter @change-filter="setFilters"></coach-filter>
+    </section>
+    <section>
+      <base-card>
+        <div class="controls">
+          <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
+          <base-button v-if="!isCoach" link to="/register">Register as coach</base-button>
+        </div>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasCoaches">
+          <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+            :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas">
+          </coach-item>
+        </ul>
+        <h3 v-else>No coaches found</h3>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -41,13 +36,13 @@ export default {
   },
   data() {
     return {
-        isLoading: false,
-        error: null,
-        activeFilters: {
-            frontend: true,
-            backend: true,
-            career: true,
-        }
+      isLoading: false,
+      error: null,
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      }
     }
   },
   isCoach() {
@@ -58,13 +53,13 @@ export default {
       const coaches = this.$store.getters['coaches/coaches'];
       return coaches.filter(coach => {
         if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
-            return true;
+          return true;
         }
         if (this.activeFilters.backend && coach.areas.includes('backend')) {
-            return true;
+          return true;
         }
         if (this.activeFilters.career && coach.areas.includes('career')) {
-            return true;
+          return true;
         }
         return false;
       })
@@ -76,7 +71,7 @@ export default {
   },
   methods: {
     setFilters(updatedFilters) {
-        this.activeFilters = updatedFilters;
+      this.activeFilters = updatedFilters;
     },
     async loadCoaches(refresh = false) {
       this.isLoading = true;
@@ -91,7 +86,7 @@ export default {
   handleError() {
     this.error = null;
   },
-  created(){
+  created() {
     this.loadCoaches();
   }
 };
