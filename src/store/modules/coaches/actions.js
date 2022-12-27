54473dd6dc0df0ee0f4babc7755b03d6e7bb5ecd
Viet <viet.dang@dirox.net>
2022-12-27 22:46:59 +0700

    Attach the token after loggin the user in successfully

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,7 +9,9 @@ export default {
             areas: data.areas,
         };
 
-        const response = await fetch(`https://vue-project-a031a-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`, {
+        const token = context.rootGetters.token;
+
+        const response = await fetch(`https://vue-project-a031a-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json?auth=` + token, {
             method: 'PUT',
             body: JSON.stringify(coachData),
         });
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -10,29 +10,25 @@ export default {
             method: 'POST',
             body: JSON.stringify(newRequest),
         });
-
         const responseData = await response.json();
-
         if (!responseData.ok) {
             const error = new Error(responseData.message || 'Failed to send request');
             throw error;
         }
-
         newRequest.id = responseData.name;
-
         newRequest.coachId = payload.coachId;
-
         context.commit('addRequest', newRequest);
     },
+
     async fetchRequests(context) {
         const coachId = context.rootGetters.userId;
-        const response = await fetch(`https://vue-project-a031a-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${coachId}.json`);
+        const token = context.rootGetters.token;
+        const response = await fetch(`https://vue-project-a031a-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${coachId}.json?auth=` + token);
         const responseData = await response.json();
         if (!response.ok) {
             const error = new Error(response.message || 'Failed to send the request');
             throw error;
         }
-
         const requests = [];
         for (const key in responseData) {
             const request = {
@@ -43,9 +39,6 @@ export default {
             }
             requests.push(request);
         }
-
-        console.log({ requests });
-
         context.commit('setRequests', requests);
     }
 }
