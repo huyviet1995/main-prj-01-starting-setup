e9aa3d546db91456e5a9b8c169511bd5fb0ba10a
Viet <viet.dang@dirox.net>
2022-12-27 22:52:43 +0700

    Only show the register as coach button when the user is successfully logged in

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,7 +10,8 @@
       <base-card>
         <div class="controls">
           <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-          <base-button v-if="!isCoach" link to="/register">Register as coach</base-button>
+          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button v-if="!isCoach && isLoggedIn && !isLoading" link to="/register">Register as coach</base-button>
         </div>
         <div v-if="isLoading">
           <base-spinner></base-spinner>
@@ -49,6 +50,9 @@ export default {
     return this.$store.getters['coaches/isCoach'];
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
     filteredCoaches() {
       const coaches = this.$store.getters['coaches/coaches'];
       return coaches.filter(coach => {
