6327c526444c94f284d870725a1d126cda8462bf
Viet <viet.dang@dirox.net>
2022-12-31 22:25:28 +0700

    Refactor the login and signup functions. Move the codes inside the login and signup into a common functions

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,29 +1,17 @@
 export default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCVlygBg2OVSO24rb2t_w5Hfqy87wJOT-Q', {
-            method: "POST",
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true,
-            })
-        })
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            const error = new Error(responseData.message || 'Failed to authenticate');
-            throw error;
-        }
-
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiredIn,
-        });
+        context.dispatch('auth', { ...payload, mode: 'login' });
     },
     async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCVlygBg2OVSO24rb2t_w5Hfqy87wJOT-Q', {
+        context.dispatch('auth', { ...payload, mode: 'signup' });
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCVlygBg2OVSO24rb2t_w5Hfqy87wJOT-Q';
+        if (mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCVlygBg2OVSO24rb2t_w5Hfqy87wJOT-Q';
+        }
+        const response = await fetch(url, {
             method: "POST",
             body: JSON.stringify({
                 email: payload.email,
